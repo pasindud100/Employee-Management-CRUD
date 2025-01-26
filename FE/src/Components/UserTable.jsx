7a14ac0defa2f899d1d3af6a38908cca768b115b
Pasindu Dilshan <dilshanp5400@gmail.com>
2025-01-26 18:25:12 +0530

    Create table to dhow emplyee data. create employee add function.

---
+++ FE/src/Components/UserTable.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Tablee from './Tablee';
+import AddUser from './AddUser';
+import UpdateUser from './UpdateUser';
+
+function UserTable() {
+  return (
+<div>
+<Tablee></Tablee>
+<AddUser/>
+<UpdateUser></UpdateUser>
+
+</div>
+)
+}
+
+export default UserTable
